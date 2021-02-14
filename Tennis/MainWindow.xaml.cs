ed561b67ae8e611f278fa84c31c916dfcb62944b
VadimGorbachevsky@gmail.com <shaersorrel@gmail.com>
2021-02-14 17:39:39 +0500

    All done. Now - convert dll, and write comments and readme.

--- Tennis/MainWindow.xaml.cs
+++ Tennis/MainWindow.xaml.cs
@@ -22,11 +22,19 @@ namespace Tennis
     public partial class MainWindow : Window
     {
         TennisGame game;
+        System.Windows.Thickness default_first_player_name_place;
         public MainWindow()
         {
             InitializeComponent();
             ViewPanel viewPanel = new ViewPanel();
             viewPanel.Show();
+            foreach (Window window in Application.Current.Windows)
+            {
+                if (window.GetType() == typeof(ViewPanel))
+                {
+                    default_first_player_name_place = (window as ViewPanel).Player1_Name_small.Margin;
+                }
+            }
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -38,6 +46,74 @@ namespace Tennis
         {
             Side_1.Content = game.LeftSide().Name();
             Side_2.Content = game.RightSide().Name();
+            foreach (Window window in Application.Current.Windows)
+            {
+                if (window.GetType() == typeof(ViewPanel))
+                {
+                    if( ((window as ViewPanel).Player1_Name_small.Margin == default_first_player_name_place)  && ( game.LeftSide().Name() == game.Player_2().Name() ) )
+                    {
+                        System.Windows.Thickness buffer = (window as ViewPanel).Player1_Name_small.Margin;
+                        (window as ViewPanel).Player1_Name_small.Margin = (window as ViewPanel).Player2_Name_small.Margin;
+                        (window as ViewPanel).Player2_Name_small.Margin = buffer;
+
+                        buffer = (window as ViewPanel).Player1_ball.Margin;
+                        (window as ViewPanel).Player1_ball.Margin = (window as ViewPanel).Player2_ball.Margin;
+                        (window as ViewPanel).Player2_ball.Margin = buffer;
+
+                        buffer = (window as ViewPanel).player1_score.Margin;
+                        (window as ViewPanel).player1_score.Margin = (window as ViewPanel).player2_score.Margin;
+                        (window as ViewPanel).player2_score.Margin = buffer;
+                    }
+                    if (((window as ViewPanel).Player2_Name_small.Margin == default_first_player_name_place) && (game.LeftSide().Name() == game.Player_1().Name()))
+                    {
+                        System.Windows.Thickness buffer = (window as ViewPanel).Player2_Name_small.Margin;
+                        (window as ViewPanel).Player2_Name_small.Margin = (window as ViewPanel).Player1_Name_small.Margin;
+                        (window as ViewPanel).Player1_Name_small.Margin = buffer;
+
+                        buffer = (window as ViewPanel).Player2_ball.Margin;
+                        (window as ViewPanel).Player2_ball.Margin = (window as ViewPanel).Player1_ball.Margin;
+                        (window as ViewPanel).Player1_ball.Margin = buffer;
+
+                        buffer = (window as ViewPanel).player2_score.Margin;
+                        (window as ViewPanel).player2_score.Margin = (window as ViewPanel).player1_score.Margin;
+                        (window as ViewPanel).player1_score.Margin = buffer;
+                    }
+                    (window as ViewPanel).Set10.Content = game.result[0, 0]; (window as ViewPanel).Set11.Content = game.result[1, 0];
+                    (window as ViewPanel).Set20.Content = game.result[0, 1]; (window as ViewPanel).Set21.Content = game.result[1, 1];
+                    (window as ViewPanel).Set30.Content = game.result[0, 2]; (window as ViewPanel).Set31.Content = game.result[1, 2];
+                    (window as ViewPanel).Set40.Content = game.result[0, 3]; (window as ViewPanel).Set41.Content = game.result[1, 3];
+                    (window as ViewPanel).Set50.Content = game.result[0, 4]; (window as ViewPanel).Set51.Content = game.result[1, 4];
+                    if (game.Winner() != "Nothing")
+                    {
+                        (window as ViewPanel).winner_place.Content = game.Winner() + "is WINNER!";
+                    }
+                    if (game.Ball() == 1)
+                    {
+                        (window as ViewPanel).Player1_ball.IsChecked = true;
+                        (window as ViewPanel).Player2_ball.IsChecked = false;
+                    }
+                    if (game.Ball() == 2)
+                    {
+                        (window as ViewPanel).Player1_ball.IsChecked = false;
+                        (window as ViewPanel).Player2_ball.IsChecked = true;
+                    }
+                    if (game.Advantage() == game.Player_1().Name())
+                    {
+                        (window as ViewPanel).player1_score.Content = "AD";
+                        (window as ViewPanel).player2_score.Content = game.Player_2().Score(0);
+                    }
+                    else if (game.Advantage() == game.Player_2().Name())
+                    {
+                        (window as ViewPanel).player1_score.Content = game.Player_1().Score(0);
+                        (window as ViewPanel).player2_score.Content = "AD";
+                    }
+                    else
+                    {
+                        (window as ViewPanel).player1_score.Content = game.Player_1().Score(0);
+                        (window as ViewPanel).player2_score.Content = game.Player_2().Score(0);
+                    }
+                }
+            }
             Set10.Content = game.result[0, 0]; Set11.Content = game.result[1, 0];
             Set20.Content = game.result[0, 1]; Set21.Content = game.result[1, 1];
             Set30.Content = game.result[0, 2]; Set31.Content = game.result[1, 2];
@@ -86,6 +162,14 @@ namespace Tennis
             Player1_Name2.Content = game.Player_1().Name();
             Player2_Name1.Content = game.Player_2().Name();
             Player2_Name2.Content = game.Player_2().Name();
+            foreach (Window window in Application.Current.Windows)
+            {
+                if (window.GetType() == typeof(ViewPanel))
+                {
+                    (window as ViewPanel).Player1_Name_small.Content = game.Player_1().Name();
+                    (window as ViewPanel).Player2_Name_small.Content = game.Player_2().Name();
+                }
+            }
             UpdateAll();
         }
 
--- Tennis/ViewPanel.xaml.cs
+++ Tennis/ViewPanel.xaml.cs
@@ -23,5 +23,6 @@ namespace Tennis
         {
             InitializeComponent();
         }
+
     }
 }
